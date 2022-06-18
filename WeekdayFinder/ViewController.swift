ad599efe59b57c57aeacfaa66363c12abc219e9c
Aleksandr F <a7214370@gmail.com>
2022-06-18 11:06:53 +0300

    Setup ViewController

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -15,6 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func findDay(_ sender: UIButton) {
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(dateTF.text!)
+        dateComponents.month = Int(monthTF.text!)
+        dateComponents.year = Int(yearTF.text!)
+        
+        let dateFormatter = DateFormatter()
+        
+        
+        
+        
     }
 }
 
