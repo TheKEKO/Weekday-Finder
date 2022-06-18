3b91a0a950307b0a75c2b0cddacb2830a425923e
Aleksandr F <a7214370@gmail.com>
2022-06-18 11:16:39 +0300

    Setup touchesBegan

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -32,5 +32,9 @@ class ViewController: UIViewController {
         resultLabel.text = weekday
         
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
